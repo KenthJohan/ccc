3659ec417c20e0c5814495b04e3a25a061dc8757
Johan Söderlind Åström <kenth.johan.work@gmail.com>
2020-03-13 09:54:42 +0100

    added

---
+++ csc_debug_sp.h
@@ -0,0 +1,47 @@
+/*
+MIT License
+
+Copyright (c) 2019 CSC Project
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+https://sigrok.org/wiki/Libserialport
+
+*/
+
+#pragma once
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+#define SP_EXIT_ON_ERROR(r) sp_exit_on_error(r,__FILE__,__LINE__)
+void sp_exit_on_error (enum sp_return r, char const * file, int line)
+{
+	if (r < 0)
+	{
+		fprintf (stderr, "%s:%i: ", file, line);
+		perror (sp_last_error_message ());
+		exit (EXIT_FAILURE);
+	}
+}
