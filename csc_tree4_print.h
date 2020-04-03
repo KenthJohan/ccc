5383e3b67d3a2040d363316fc08e679173e12997
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2020-04-03 11:13:45 +0200

    updated csc_tree4_print.h

--- csc_tree4_print.h
+++ csc_tree4_print.h
@@ -28,14 +28,16 @@ SOFTWARE.
 #include "csc_tcol.h"
 #include "csc_tree4.h"
 
+typedef void (*csc_tree4_print_traverse_cb)(struct csc_tree4 const * node, void *ptr);
 
 
-void csc_tree4_print_traverse
-(
-struct csc_tree4 const * node,
-void * ptr,
-char const * (*cb)(struct csc_tree4 const * node, void *ptr)
-)
+/**
+ * @brief Print pretty left-child right-sibling binary tree.
+ * @param node Traverse the tree from this node downwards.
+ * @param ptr User pointer that is passed on to the callback function \p cb.
+ * @param cb A callback function that is called for each node in the tree.
+ */
+void csc_tree4_print_traverse (struct csc_tree4 const * node, void * ptr, csc_tree4_print_traverse_cb cb)
 {
 	char vertical[] = TCOL(TCOL_NORMAL,TCOL_GREEN,TCOL_DEFAULT) "\u2502" TCOL_RST;
 	char cross[] = TCOL(TCOL_NORMAL,TCOL_GREEN,TCOL_DEFAULT) "\u251C" TCOL_RST;
@@ -70,7 +72,7 @@ char const * (*cb)(struct csc_tree4 const * node, void *ptr)
 			indent &= ~(1 << depth);
 		}
 		printf ("%s", dash);
-		puts (cb (node, ptr));
+		cb (node, ptr);
 		node = csc_tree4_traverse (node, &depth);
 	}
 }
