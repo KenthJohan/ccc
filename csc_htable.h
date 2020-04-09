1661fe6615b788a1e240582a130a092031ab1c47
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2020-04-10 00:29:07 +0200

    fixed csc_htable.h

--- csc_htable.h
+++ csc_htable.h
@@ -29,7 +29,7 @@ SOFTWARE.
 #include "csc_basic.h"
 
 
-unsigned hash8_buf (void const * buf, unsigned size8, unsigned max)
+unsigned csc_hash8_buf (void const * buf, unsigned size8, unsigned max)
 {
 	ASSERT_ISPOW2 (max);
 	unsigned key = 0;
@@ -42,7 +42,7 @@ unsigned hash8_buf (void const * buf, unsigned size8, unsigned max)
 }
 
 
-unsigned hash8_str (char const * buf, char const * buf_end, unsigned max)
+unsigned csc_hash8_str (char const * buf, char const * buf_end, unsigned max)
 {
 	ASSERT_ISPOW2 (max);
 	unsigned key = 0;
@@ -57,7 +57,7 @@ unsigned hash8_str (char const * buf, char const * buf_end, unsigned max)
 }
 
 
-unsigned hash8_str_ab (char const * a, char const * b, unsigned max)
+unsigned csc_hash8_str_ab (char const * a, char const * b, unsigned max)
 {
 	ASSERT_ISPOW2 (max);
 	unsigned key = 0;
@@ -72,6 +72,117 @@ unsigned hash8_str_ab (char const * a, char const * b, unsigned max)
 }
 
 
+
+struct csc_htablestr
+{
+	struct csc_dlist * slots; //Hashtable
+	struct csc_dlist * node; //node+i*stride, can be inserted to the hashtable
+	char * name; //name+i*stride
+	unsigned htsize;
+	unsigned items_max;
+	unsigned name_stride;
+	unsigned last;
+};
+
+
+
+void csc_htablestr_init (struct csc_htablestr * ht)
+{
+	ASSERT_PARAM_NOTNULL (ht);
+	ASSERT_ISPOW2 (ht->htsize);
+	//Reserve element 0 for miscs. Thus skip element 1.
+	ht->last = 1;
+	ht->slots = calloc (ht->htsize, sizeof (struct csc_dlist));
+	ht->node = calloc (ht->items_max, sizeof (struct csc_dlist));
+	ht->name = calloc (ht->items_max * ht->name_stride, sizeof (char));
+	csc_dlist_init_v (ht->slots, ht->htsize);
+}
+
+
+struct csc_dlist * csc_htablestr_get_nth_node (struct csc_htablestr * ht, unsigned index)
+{
+	ASSERT (index <= ht->last);
+	return ht->node + index;
+}
+
+
+char * csc_htablestr_get_nth_name (struct csc_htablestr * ht, unsigned index)
+{
+	ASSERT (index <= ht->last);
+	return ht->name + ht->name_stride * index;
+}
+
+
+struct csc_dlist * csc_htablestr_get_last_node (struct csc_htablestr * ht)
+{
+	return csc_htablestr_get_nth_node (ht, ht->last);
+}
+
+
+char * csc_htablestr_get_last_name (struct csc_htablestr * ht)
+{
+	return csc_htablestr_get_nth_name (ht, ht->last);
+}
+
+
+int csc_htablestr_add (struct csc_htablestr * ht, char const * str, char const * str_end)
+{
+	ASSERT_PARAM_NOTNULL (ht);
+	ASSERT_PARAM_NOTNULL (str);
+	ASSERT_ISPOW2 (ht->htsize);
+	ASSERT (ht->slots);
+	ASSERT (ht->node);
+	ASSERT (ht->name);
+	if (ht->last >= ht->items_max)
+	{
+		puts ("");
+	}
+	ASSERT (ht->last < ht->items_max);
+	struct csc_dlist * nodeslot = csc_htablestr_get_last_node (ht);
+	char * name = csc_htablestr_get_last_name (ht);
+	if (str_end)
+	{
+		ptrdiff_t l = MIN (str_end - str, ht->name_stride);
+		memcpy (name, str, (size_t)l);
+		ASSERT (strlen (name) <= (size_t)l);
+	}
+	else
+	{
+		snprintf (name, ht->name_stride, "%s", str);
+	}
+	unsigned key = csc_hash8_str (name, NULL, ht->htsize);
+	csc_dlist_add_head (ht->slots + key, nodeslot);
+	int index = (int)ht->last;
+	ht->last++;
+	return index;
+}
+
+
+int csc_htablestr_find (struct csc_htablestr const * ht, char const * needle, char const * needle_end)
+{
+	ASSERT_PARAM_NOTNULL (ht);
+	ASSERT_PARAM_NOTNULL (needle);
+	ASSERT_ISPOW2 (ht->htsize);
+	ASSERT (ht->slots);
+	ASSERT (ht->node);
+	ASSERT (ht->name);
+	unsigned key = csc_hash8_str (needle, needle_end, ht->htsize);
+	struct csc_dlist * p = ht->slots + key;
+	return csc_dlist_find_str (p, ht->node, needle, needle_end, ht->name, ht->name_stride, 0);
+}
+
+
+int csc_htablestr_getfind (struct csc_htablestr * ht, char const * needle, char const * needle_end)
+{
+	int index;
+	index = csc_htablestr_find (ht, needle, needle_end);
+	if (index < 0)
+	{
+		index = csc_htablestr_add (ht, needle, needle_end);
+	}
+	ASSERT (index >= 0);
+	return index;
+}
 
 
 
--- test_csc_htable.c
+++ test_csc_htable.c
@@ -4,64 +4,48 @@
 #include "csc_basic.h"
 
 
-#define BUCKETS_COUNT 100
-#define BUCKETS_NAME_COUNT 10
-#define BUCKETS_HT_COUNT 4
 
-
-struct buckets
-{
-	struct csc_dlist * ht; //Hashtable
-	struct csc_dlist * node; //node+i*stride, can be inserted to the hashtable
-	char * name; //name+i*stride
-	int * count; //count+i*stride
-};
-
-void buckets_set (struct buckets * b, unsigned i, char const * str, int count)
-{
-	char * name = b->name + BUCKETS_NAME_COUNT * i;
-	struct csc_dlist * node = b->node + i;
-	snprintf (name, BUCKETS_NAME_COUNT, "%s", str);
-	b->count [i] = count;
-	uint8_t key = hash8_str (name, BUCKETS_HT_COUNT);
-	ASSERT (node->next == NULL);
-	ASSERT (node->prev == NULL);
-	csc_dlist_add_head (b->ht + key, node);
-}
-
-int buckets_find (struct buckets * b, char const * needle)
-{
-	uint8_t key = hash8_str (needle, BUCKETS_HT_COUNT);
-	struct csc_dlist * p = b->ht + key;
-	int i = csc_dlist_find_str (p, b->node, needle, b->name, BUCKETS_NAME_COUNT, 0);
-	return i;
-}
 
 int main (int argc, char * argv [])
 {
 	ASSERT (argc);
 	ASSERT (argv);
+	setbuf (stdout, NULL);
 
-	struct buckets b;
-	b.ht = calloc (BUCKETS_HT_COUNT, sizeof (struct csc_dlist));
-	b.node = calloc (BUCKETS_COUNT, sizeof (struct csc_dlist));
-	b.name = calloc (BUCKETS_COUNT * BUCKETS_NAME_COUNT, sizeof (char));
-	b.count = calloc (BUCKETS_COUNT, sizeof (int));
+	struct csc_htablestr ht;
+	ht.htsize = 4;
+	ht.name_stride = 20;
+	ht.items_max = 30;
+	csc_htablestr_init (&ht);
 
-	csc_dlist_init_v (b.ht, BUCKETS_HT_COUNT);
+	csc_htablestr_add (&ht, "Apple", NULL);
+	csc_htablestr_add (&ht, "Banana", NULL);
+	csc_htablestr_add (&ht, "Pear", NULL);
+	csc_htablestr_add (&ht, "Orange", NULL);
+	csc_htablestr_add (&ht, "Grape", NULL);
+	csc_htablestr_add (&ht, "Lemon", NULL);
+	csc_htablestr_add (&ht, "Lime", NULL);
+	csc_htablestr_add (&ht, "Onion", NULL);
+	ASSERT (ht.last >= 7);
 
-	buckets_set (&b, 0, "Banana", 200);
-	buckets_set (&b, 1, "Apples", 300);
-	buckets_set (&b, 2, "Lemons", 500);
-	buckets_set (&b, 3, "Oranges", 200);
-	buckets_set (&b, 4, "Pears", 150);
+	ASSERT (csc_htablestr_find (&ht, "Nonexist", NULL) < 0);
+	ASSERT (csc_htablestr_find (&ht, "Apple", NULL) >= 0);
+	ASSERT (csc_htablestr_find (&ht, "Banana", NULL) >= 0);
+	ASSERT (csc_htablestr_find (&ht, "Pear", NULL) >= 0);
+	ASSERT (csc_htablestr_find (&ht, "Orange", NULL) >= 0);
+	ASSERT (csc_htablestr_find (&ht, "Grape", NULL) >= 0);
+	ASSERT (csc_htablestr_find (&ht, "Lemon", NULL) >= 0);
+	ASSERT (csc_htablestr_find (&ht, "Lime", NULL) >= 0);
+	ASSERT (csc_htablestr_find (&ht, "Onion", NULL) >= 0);
 
-	ASSERT (csc_dlist_empty_v (b.ht, BUCKETS_HT_COUNT) != 0);
-	ASSERT (csc_dlist_nonempty_v (b.ht, BUCKETS_HT_COUNT) != 0);
-	ASSERT (csc_dlist_empty_v (b.ht, BUCKETS_HT_COUNT) + csc_dlist_nonempty_v (b.ht, BUCKETS_HT_COUNT) == BUCKETS_HT_COUNT);
-
-	ASSERT (buckets_find (&b, "Lemons") >= 0);
-	ASSERT (strcmp (b.name + buckets_find (&b, "Lemons") * BUCKETS_NAME_COUNT, "Lemons") == 0);
+	ASSERT (strcmp (csc_htablestr_get_nth_name (&ht, (unsigned)csc_htablestr_find (&ht, "Apple", NULL)), "Apple") == 0);
+	ASSERT (strcmp (csc_htablestr_get_nth_name (&ht, (unsigned)csc_htablestr_find (&ht, "Banana", NULL)), "Banana") == 0);
+	ASSERT (strcmp (csc_htablestr_get_nth_name (&ht, (unsigned)csc_htablestr_find (&ht, "Pear", NULL)), "Pear") == 0);
+	ASSERT (strcmp (csc_htablestr_get_nth_name (&ht, (unsigned)csc_htablestr_find (&ht, "Orange", NULL)), "Orange") == 0);
+	ASSERT (strcmp (csc_htablestr_get_nth_name (&ht, (unsigned)csc_htablestr_find (&ht, "Grape", NULL)), "Grape") == 0);
+	ASSERT (strcmp (csc_htablestr_get_nth_name (&ht, (unsigned)csc_htablestr_find (&ht, "Lemon", NULL)), "Lemon") == 0);
+	ASSERT (strcmp (csc_htablestr_get_nth_name (&ht, (unsigned)csc_htablestr_find (&ht, "Lime", NULL)), "Lime") == 0);
+	ASSERT (strcmp (csc_htablestr_get_nth_name (&ht, (unsigned)csc_htablestr_find (&ht, "Onion", NULL)), "Onion") == 0);
 
 	return EXIT_SUCCESS;
 }
