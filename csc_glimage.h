029cb90bf5538040906a1b3938ab021052e0e133
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2021-01-31 20:10:43 +0100

    Added some opengl test concept

---
+++ csc_glimage.h
@@ -0,0 +1,178 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#pragma once
+#include <stddef.h> //offsetof
+#include <stdint.h> //offsetof
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include "csc_basic.h"
+#include "csc_debug.h"
+
+
+
+struct csc_rectangle_v4f32
+{
+	float a[4];
+	float b[4];
+	float c[4];
+	float d[4];
+	float e[4];
+	float f[4];
+};
+
+
+void csc_rectangle_setcoord_v4f32 (struct csc_rectangle_v4f32 * item, float x, float y, float z, float w, float h)
+{
+	float wr = w/2.0f;
+	float hr = h/2.0f;
+
+	item->a[0] = -wr + x;
+	item->a[1] = -hr + y;
+	item->a[2] = z;
+	//item->a[3] = 1.0f;
+
+	item->b[0] = wr + x;
+	item->b[1] = -hr + y;
+	item->b[2] = z;
+	//item->b[3] = 1.0f;
+
+	item->c[0] = wr + x;
+	item->c[1] = hr + y;
+	item->c[2] = z;
+	//item->c[3] = 1.0f;
+
+	item->d[0] = -wr + x;
+	item->d[1] = -hr + y;
+	item->d[2] = z;
+	//item->d[3] = 1.0f;
+
+	item->e[0] = wr + x;
+	item->e[1] = hr + y;
+	item->e[2] = z;
+	//item->e[3] = 1.0f;
+
+	item->f[0] = -wr + x;
+	item->f[1] = hr + y;
+	item->f[2] = z;
+	//item->f[3] = 1.0f;
+}
+
+
+
+
+void csc_rectangle_setuv_vf32 (float uv[], uint32_t n)
+{
+	for (uint32_t i = 0; i < n; ++i)
+	{
+		uv[0] = 0.0f;
+		uv[1] = 0.0f;
+
+		uv[2] = 1.0f;
+		uv[3] = 0.0f;
+
+		uv[4] = 1.0f;
+		uv[5] = 1.0f;
+
+		uv[6] = 0.0f;
+		uv[7] = 0.0f;
+
+		uv[8] = 1.0f;
+		uv[9] = 1.0f;
+
+		uv[10] = 0.0f;
+		uv[11] = 1.0f;
+		uv += 12;
+	}
+}
+
+
+#define CSC_GLIMAGE_POS_DIM 4
+#define CSC_GLIMAGE_TEX_DIM 2
+#define CSC_GLIMAGE_VERTS_COUNT 6
+
+
+struct csc_glimage
+{
+	uint32_t cap;
+	GLuint vbop;
+	GLuint vbot;
+	GLuint vao;
+	float * vpos;
+	float * vtex;
+
+	GLint glprogram;
+	GLint uniform_mvp;
+	GLint uniform_texture1;
+};
+
+
+void csc_glimage_init (struct csc_glimage * img)
+{
+	img->uniform_mvp = glGetUniformLocation (img->glprogram, "mvp");
+	img->uniform_texture1 = glGetUniformLocation (img->glprogram, "texture1");
+	ASSERT (img->uniform_mvp >= 0);
+	ASSERT (img->uniform_texture1 >= 0);
+
+	img->vpos = calloc (img->cap * CSC_GLIMAGE_VERTS_COUNT * CSC_GLIMAGE_POS_DIM, sizeof(float));
+	img->vtex = calloc (img->cap * CSC_GLIMAGE_VERTS_COUNT * CSC_GLIMAGE_TEX_DIM, sizeof(float));
+
+	glGenVertexArrays (1, &img->vao);
+	glGenBuffers (1, &img->vbop);
+	glGenBuffers (1, &img->vbot);
+
+	glBindVertexArray (img->vao);
+
+	glBindBuffer (GL_ARRAY_BUFFER, img->vbop);
+	glBufferData (GL_ARRAY_BUFFER, img->cap * CSC_GLIMAGE_VERTS_COUNT * CSC_GLIMAGE_POS_DIM * sizeof(float), NULL, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer (0, CSC_GLIMAGE_POS_DIM, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray (0);
+
+	glBindBuffer (GL_ARRAY_BUFFER, img->vbot);
+	csc_rectangle_setuv_vf32 (img->vtex, img->cap);
+	glBufferData (GL_ARRAY_BUFFER, img->cap * CSC_GLIMAGE_VERTS_COUNT * CSC_GLIMAGE_TEX_DIM * sizeof(float), img->vtex, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer (1, CSC_GLIMAGE_TEX_DIM, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray (1);
+}
+
+
+void csc_glimage_update_xywh (struct csc_glimage * img, float xyzw[], float wh[], uint32_t n)
+{
+	float * vpos = img->vpos;
+	for (uint32_t i = 0; i < n; ++i)
+	{
+		csc_rectangle_setcoord_v4f32((void*)vpos, xyzw[0], xyzw[1], xyzw[2], wh[0], wh[1]);
+		xyzw += 4;
+		wh += 2;
+		vpos += CSC_GLIMAGE_POS_DIM*CSC_GLIMAGE_VERTS_COUNT;
+	}
+	glBindBuffer (GL_ARRAY_BUFFER, img->vbop);
+	glBufferSubData (GL_ARRAY_BUFFER, 0, img->cap * CSC_GLIMAGE_VERTS_COUNT * CSC_GLIMAGE_POS_DIM * sizeof (float), img->vpos);
+}
+
+
+
+void csc_glimage_draw (struct csc_glimage * img, float mvp[4*4])
+{
+	glBindVertexArray (img->vao);
+	glUseProgram (img->glprogram);
+	glUniform1i (img->uniform_texture1, 0);
+	glUniformMatrix4fv (img->uniform_mvp, 1, GL_FALSE, (const GLfloat *) mvp);
+	glDrawArrays (GL_TRIANGLES, 0, img->cap * CSC_GLIMAGE_VERTS_COUNT);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ csc_glpointcloud.h
@@ -0,0 +1,100 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#pragma once
+#include <stddef.h> //offsetof
+#include <stdint.h> //offsetof
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include "csc_basic.h"
+#include "csc_debug.h"
+
+
+
+#define CSC_GLPOINTCLOUD_POS_DIM 4
+#define CSC_GLPOINTCLOUD_COL_DIM 4
+
+
+struct csc_glpointcloud
+{
+	uint32_t cap;
+	GLuint vbop;
+	GLuint vboc;
+	GLuint vao;
+	float * vpos;
+	uint32_t * vcol;
+	GLint glprogram;
+	GLint uniform_mvp;
+};
+
+
+void csc_glpointcloud_init (struct csc_glpointcloud * item)
+{
+	ASSERT (item->glprogram > 0);
+	item->uniform_mvp = glGetUniformLocation (item->glprogram, "mvp");
+	ASSERT (item->uniform_mvp >= 0);
+
+	item->vpos = calloc (item->cap * CSC_GLPOINTCLOUD_POS_DIM, sizeof(float));
+	item->vcol = calloc (item->cap * CSC_GLPOINTCLOUD_COL_DIM, sizeof(uint32_t));
+
+	float * p = item->vpos;
+	for (uint32_t i = 0; i < item->cap; ++i)
+	{
+		p[0] = (((float)rand() / (float)RAND_MAX)-0.5f) * 1.0f;
+		p[1] = (((float)rand() / (float)RAND_MAX)-0.5f) * 1.0f;
+		p[2] = (((float)rand() / (float)RAND_MAX)-0.5f) * 1.0f;
+		p[3] = 1.0f;
+		item->vcol[i] = 0xFFFFAAFF;
+		p += CSC_GLPOINTCLOUD_POS_DIM;
+	}
+
+
+	glGenVertexArrays (1, &item->vao);
+	glGenBuffers (1, &item->vbop);
+	glGenBuffers (1, &item->vboc);
+
+	glBindVertexArray (item->vao);
+
+	glBindBuffer (GL_ARRAY_BUFFER, item->vbop);
+	glBufferData (GL_ARRAY_BUFFER, item->cap * CSC_GLPOINTCLOUD_POS_DIM * sizeof(float), item->vpos, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer (0, CSC_GLPOINTCLOUD_POS_DIM, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray (0);
+
+	glBindBuffer (GL_ARRAY_BUFFER, item->vboc);
+	glBufferData (GL_ARRAY_BUFFER, item->cap * CSC_GLPOINTCLOUD_COL_DIM * sizeof(uint32_t), item->vcol, GL_DYNAMIC_DRAW);
+	glVertexAttribPointer (1, CSC_GLPOINTCLOUD_COL_DIM, GL_UNSIGNED_BYTE, GL_TRUE, 0, (void*)0);
+	glEnableVertexAttribArray (1);
+}
+
+
+void csc_glpointcloud_update (struct csc_glpointcloud * item)
+{
+	glBindBuffer (GL_ARRAY_BUFFER, item->vbop);
+	glBufferSubData (GL_ARRAY_BUFFER, 0, item->cap * CSC_GLPOINTCLOUD_POS_DIM * sizeof (float), item->vpos);
+}
+
+
+
+void csc_glpointcloud_draw (struct csc_glpointcloud * item, float mvp[4*4])
+{
+	glBindVertexArray (item->vao);
+	glUseProgram (item->glprogram);
+	glUniformMatrix4fv (item->uniform_mvp, 1, GL_FALSE, (const GLfloat *) mvp);
+	glDrawArrays (GL_POINTS, 0, item->cap);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
