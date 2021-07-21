bab06dc24ffdc4582fc4700c1d202a0f73e67ce6
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2021-07-21 22:51:07 +0200

    test

--- csc_debug.h
+++ csc_debug.h
@@ -11,56 +11,62 @@ SPDX-FileCopyrightText: 2021 Johan Söderlind Åström <johan.soderlind.astrom@g
 #include "csc_tcol.h"
 
 
-
-#define TRACE_TCOL_INFO0 TCOL (TCOL_NORMAL, TCOL_YELLOW, TCOL_DEFAULT)
-#define TRACE_TCOL_INFO1 TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT)
-#define TRACE_TCOL_INFO2 TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT)
-#define TRACE_TCOL_INFO3 TCOL (TCOL_NORMAL, TCOL_RED, TCOL_DEFAULT)
-#define TRACE_TCOL_INFO4 TCOL (TCOL_NORMAL, TCOL_BLUE, TCOL_DEFAULT)
-#define TRACE_TCOL_INFO5 TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT)
-#define TRACE_TCOL_INFO6 TCOL (TCOL_NORMAL, TCOL_BLUE, TCOL_DEFAULT)
-#define ASSERT_TCOL0 TCOL (TCOL_NORMAL, TCOL_RED, TCOL_DEFAULT)
-#define ASSERT_TCOL1 TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT)
-#define ASSERT_TCOL2 TCOL (TCOL_NORMAL, TCOL_YELLOW, TCOL_DEFAULT)
-#define ASSERT_TCOL3 TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT)
-#define ASSERT_TCOL4 TCOL (TCOL_NORMAL, TCOL_CYAN, TCOL_DEFAULT)
-#define ASSERT_TCOL5 TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT)
-#define ASSERT_TCOL6 TCOL (TCOL_BOLD, TCOL_RED, TCOL_DEFAULT)
-#define ASSERT_TCOLID TCOL (TCOL_NORMAL, TCOL_GREEN, TCOL_DEFAULT)
-
-
-/*
-	//https://stackoverflow.com/questions/8487986/file-macro-shows-full-path
-	#ifdef _WIN32
-	#define CSC_RELATIVE_FILE (strrchr("\\" __FILE__, '\\') + 1)
-	#else
-	#define CSC_RELATIVE_FILE (strrchr("/" __FILE__, '/') + 1)
-	#endif
-*/
-
-
 //https://gcc.gnu.org/onlinedocs/gcc/Variadic-Macros.html
 //http://www.open-std.org/jtc1/sc22/wg14/www/docs/n2153.htm
 #define ASSERT_CARGS __COUNTER__, __FILE__, __LINE__, __func__
 
+#define ASSERT_STYLE_VARNAME    TFG(123,200,255) "%s"              TCOL_RST
+#define ASSERT_STYLE_I          TFG(123,123,255) "%jd"             TCOL_RST
+#define ASSERT_STYLE_U          TFG(123,123,255) "%ju"             TCOL_RST
+#define ASSERT_STYLE_P          TFG(123,123,255) "%p"            TCOL_RST
+#define ASSERT_STYLE_NULL       TFG(123,123,255) "NULL"            TCOL_RST
+#define ASSERT_STYLE_FALSE      TFG(123,123,255) "FALSE"            TCOL_RST
+#define ASSERT_STYLE_TRUE       TFG(123,123,255) "TRUE"            TCOL_RST
+#define ASSERT_STYLE_ASSERT     TFG(255,66,66)  "A%04x"           TCOL_RST
+#define ASSERT_STYLE_FILENAME   TFG(99,99,99)   "%s"              TCOL_RST
+#define ASSERT_STYLE_LINE       TFG(99,99,99)   "%i"              TCOL_RST
+#define ASSERT_STYLE_FNAME      TFG(99,120,99)  "%s"              TCOL_RST
+#define ASSERT_STYLE_ERRNO      TFG(190,90,90)  "%s (errno=%i)"   TCOL_RST
+
+#define ASSERT_STYLE_NOTNULL         "Failed assertion: (" ASSERT_STYLE_VARNAME " /= "  ASSERT_STYLE_NULL    ") evaluated as (" ASSERT_STYLE_P " /= " ASSERT_STYLE_NULL ")"
+#define ASSERT_STYLE_PARAM_NOTNULL   "Failed assertion: Paramater (" ASSERT_STYLE_VARNAME " /= "  ASSERT_STYLE_NULL    ") evaluated as (" ASSERT_STYLE_P " /= " ASSERT_STYLE_NULL ")"
+#define ASSERT_STYLE_ISFALSE         "Failed assertion: (" ASSERT_STYLE_VARNAME " = "   ASSERT_STYLE_FALSE    ") evaluated as (" ASSERT_STYLE_P " = " ASSERT_STYLE_FALSE ")"
+#define ASSERT_STYLE_ISTRUE          "Failed assertion: (" ASSERT_STYLE_VARNAME " = "   ASSERT_STYLE_TRUE     ") evaluated as (" ASSERT_STYLE_P " = " ASSERT_STYLE_TRUE ")"
+#define ASSERT_STYLE_ISPOW2          "Failed assertion: (" ASSERT_STYLE_VARNAME " = "   ASSERT_STYLE_U ") is a positive integer power of two."
+#define ASSERT_STYLE_ISALIGN         "Failed assertion: (" ASSERT_STYLE_VARNAME " = "   ASSERT_STYLE_P") aligned to ("ASSERT_STYLE_VARNAME" = "ASSERT_STYLE_U"), "ASSERT_STYLE_U
+#define ASSERT_STYLE_LTI             "Failed assertion: (" ASSERT_STYLE_VARNAME " < "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_I " < "  ASSERT_STYLE_I	  ")"
+#define ASSERT_STYLE_LTU             "Failed assertion: (" ASSERT_STYLE_VARNAME " < "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_U " < "  ASSERT_STYLE_U	  ")"
+#define ASSERT_STYLE_LTP             "Failed assertion: (" ASSERT_STYLE_VARNAME " < "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_P " < "  ASSERT_STYLE_P    ")"
+#define ASSERT_STYLE_GTI             "Failed assertion: (" ASSERT_STYLE_VARNAME " > "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_I " > "  ASSERT_STYLE_I    ")"
+#define ASSERT_STYLE_GTU             "Failed assertion: (" ASSERT_STYLE_VARNAME " > "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_U " > "  ASSERT_STYLE_U    ")"
+#define ASSERT_STYLE_GTP             "Failed assertion: (" ASSERT_STYLE_VARNAME " > "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_P " > "  ASSERT_STYLE_P    ")"
+#define ASSERT_STYLE_EQI             "Failed assertion: (" ASSERT_STYLE_VARNAME " = "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_I " = "  ASSERT_STYLE_I    ")"
+#define ASSERT_STYLE_EQU             "Failed assertion: (" ASSERT_STYLE_VARNAME " = "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_U " = "  ASSERT_STYLE_U    ")"
+#define ASSERT_STYLE_EQP             "Failed assertion: (" ASSERT_STYLE_VARNAME " = "   ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_P " = "  ASSERT_STYLE_P    ")"
+#define ASSERT_STYLE_NEQI            "Failed assertion: (" ASSERT_STYLE_VARNAME " /= "  ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_I " /= "  ASSERT_STYLE_I    ")"
+#define ASSERT_STYLE_NEQU            "Failed assertion: (" ASSERT_STYLE_VARNAME " /= "  ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_U " /= "  ASSERT_STYLE_U    ")"
+#define ASSERT_STYLE_NEQP            "Failed assertion: (" ASSERT_STYLE_VARNAME " /= "  ASSERT_STYLE_VARNAME ") evaluated as (" ASSERT_STYLE_P " /= "  ASSERT_STYLE_P    ")"
+
 #define ASSERT(A)               do{if(!(A)){assert_format(ASSERT_CARGS, #A, (NULL)             );}}while(0)
 #define ASSERTF(A, F, ...)      do{if(!(A)){assert_format(ASSERT_CARGS, #A, (F), ## __VA_ARGS__);}}while(0)
-#define ASSERT_NOTNULL(A)       do{if((A)==NULL){assert_format(ASSERT_CARGS, NULL, "variable " ASSERT_TCOLID "%s" TCOL_RST " can not be NULL", #A);}}while(0)
-#define ASSERT_LT(A,B)          do{if((A)>=(B)){assert_format(ASSERT_CARGS, NULL, "number " ASSERT_TCOLID "%s=%jd" TCOL_RST " must be less than " ASSERT_TCOLID "%s=%jd" TCOL_RST, #A, (intmax_t)(A), #B, (intmax_t)(B));}}while(0)
-#define ASSERT_GT(A,B)          do{if((A)<=(B)){assert_format(ASSERT_CARGS, NULL, "number " ASSERT_TCOLID "%s=%jd" TCOL_RST " must be larger than " ASSERT_TCOLID "%s=%jd" TCOL_RST, #A, (intmax_t)(A), #B, (intmax_t)(B));}}while(0)
-#define ASSERT_PTR_NEQ(A,B)     do{if((char*)(A)!=(char*)(B)){assert_format(ASSERT_CARGS, NULL, "pointer " ASSERT_TCOLID "%s" TCOL_RST " must not be equal " ASSERT_TCOLID "%s" TCOL_RST, #A);}}while(0)
-#define ASSERT_PTR_EQ(A,B)      do{if((char*)(A)==(char*)(B)){assert_format(ASSERT_CARGS, NULL, "pointer " ASSERT_TCOLID "%s" TCOL_RST " must be equal " ASSERT_TCOLID "%s" TCOL_RST, #A);}}while(0)
-#define ASSERT_PTR_LT(A,B)      do{if((char*)(A)<(char*)(B)){assert_format(ASSERT_CARGS, NULL, "pointer " ASSERT_TCOLID "%s" TCOL_RST " must be less than " ASSERT_TCOLID "%s" TCOL_RST, #A);}}while(0)
-#define ASSERT_PTR_GT(A,B)      do{if((char*)(A)>(char*)(B)){assert_format(ASSERT_CARGS, NULL, "pointer " ASSERT_TCOLID "%s" TCOL_RST " must be greater than " ASSERT_TCOLID "%s" TCOL_RST, #A);}}while(0)
-#define ASSERT_FALSE(A)         do{if((A)==0){assert_format(ASSERT_CARGS, NULL, "variable " ASSERT_TCOLID "%s" TCOL_RST " can not be false", #A);}}while(0)
-#define ASSERT_PARAM_NOTNULL(A) do{if((A)==NULL){assert_format(ASSERT_CARGS, NULL, "parameter " ASSERT_TCOLID "%s" TCOL_RST " can not be NULL", #A);}}while(0)
-#define ASSERT_ISPOW2(x)        ASSERTF((x & (x-1)) == 0, "The number %i must be a positive integer power of two", (int)(x))
-#define ASSERT_ISALIGN(x,a)     ASSERTF(((uintptr_t)(void const*)(x) % (a)) == 0, "The address %p must be align to %i, %u", (void const *)(x), (int)(a), (uintptr_t)(void const*)(x) % (a))
-
-#define TRACE(F)           trace_format (ASSERT_CARGS, (F)                )
-#define TRACEF(F, ...)     trace_format (ASSERT_CARGS, (F), ## __VA_ARGS__)
-
-
+#define ASSERT_NOTNULL(A)       do{if(!(A))       {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_NOTNULL,        #A, (A));}}while(0)
+#define ASSERT_PARAM_NOTNULL(A) do{if(!(A))       {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_PARAM_NOTNULL,  #A, (A));}}while(0)
+#define ASSERT_LTI(A,B)         do{if((A)>=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_LTI,            #A, #B,  (intmax_t)(A),  (intmax_t)(B));}}while(0)
+#define ASSERT_LTU(A,B)         do{if((A)>=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_LTU,            #A, #B, (uintmax_t)(A), (uintmax_t)(B));}}while(0)
+#define ASSERT_LTP(A,B)         do{if((A)>=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_LTP,            #A, #B,            (A),            (B));}}while(0)
+#define ASSERT_GTI(A,B)         do{if((A)<=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_GTI,            #A, #B,  (intmax_t)(A),  (intmax_t)(B));}}while(0)
+#define ASSERT_GTU(A,B)         do{if((A)<=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_GTU,            #A, #B, (uintmax_t)(A), (uintmax_t)(B));}}while(0)
+#define ASSERT_GTP(A,B)         do{if((A)<=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_GTP,            #A, #B,            (A),            (B));}}while(0)
+#define ASSERT_EQI(A,B)         do{if((A)!=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_EQI,            #A, #B,  (intmax_t)(A),  (intmax_t)(B));}}while(0)
+#define ASSERT_EQU(A,B)         do{if((A)!=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_EQU,            #A, #B, (uintmax_t)(A), (uintmax_t)(B));}}while(0)
+#define ASSERT_EQP(A,B)         do{if((A)!=(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_EQP,            #A, #B,            (A),            (B));}}while(0)
+#define ASSERT_NEQI(A,B)        do{if((A)==(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_NEQI,           #A, #B,  (intmax_t)(A),  (intmax_t)(B));}}while(0)
+#define ASSERT_NEQU(A,B)        do{if((A)==(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_NEQU,           #A, #B, (uintmax_t)(A), (uintmax_t)(B));}}while(0)
+#define ASSERT_NEQP(A,B)        do{if((A)==(B))   {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_NEQP,           #A, #B,            (A),            (B));}}while(0)
+#define ASSERT_FALSE(A)         do{if((A))        {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_ISFALSE,        #A, (A));}}while(0)
+#define ASSERT_TRUE(A)          do{if((A)!=0)     {assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_ISTRUE,         #A, (A));}}while(0)
+#define ASSERT_POW2(A)          do{if((A)&((A)-1)){assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_ISPOW2,         #A, (A));}}while(0)
+#define ASSERT_ALIGN(x,a)       do{if(((uintptr_t)(void const*)(x) % (a)) != 0){assert_format(ASSERT_CARGS, NULL, ASSERT_STYLE_ISALIGN, #a, (a), #x, (x), (uintptr_t)(void const*)(x) % (a));}}while(0)
 
 
 
@@ -68,20 +74,12 @@ SPDX-FileCopyrightText: 2021 Johan Söderlind Åström <johan.soderlind.astrom@g
 __attribute__ ((unused))
 __attribute__ ((format (printf, 6, 0)))
 static void assert_format_va
-(
-int id,
-char const * file,
-int line,
-char const * fn,
-char const * exp,
-char const * fmt,
-va_list va
-)
+(int id, char const * file, int line, char const * fn, char const * exp, char const * fmt, va_list va)
 {
-	fprintf (stderr, ASSERT_TCOL0 "ASSERT%04i" TCOL_RST " ", id);
-	fprintf (stderr, TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT) "%s:%04i" TCOL_RST " in ", file, line);
-	fprintf (stderr, ASSERT_TCOLID "%s() " TCOL_RST, fn);
-	if (errno != 0) {fprintf (stderr, "[%04i:" TCOL (TCOL_BOLD, TCOL_RED , TCOL_DEFAULT) "%s" TCOL_RST "]: ", errno, strerror (errno));}
+	fprintf (stderr, ASSERT_STYLE_ASSERT " " ASSERT_STYLE_FILENAME ":" ASSERT_STYLE_LINE " " ASSERT_STYLE_FNAME "()", id, file, line, fn);
+	//fprintf (stderr, TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT) "%s:%04i" TCOL_RST " in ", file, line);
+	//fprintf (stderr, ASSERT_TCOLID "%s() " TCOL_RST, fn);
+	if (errno != 0) {fprintf (stderr, " " ASSERT_STYLE_ERRNO " ", strerror (errno), errno);}
 	if (exp){fprintf (stderr, TCOL (TCOL_NORMAL, TCOL_WHITE, TCOL_DEFAULT) "%s" TCOL_RST " ", exp);}
 	if (fmt)
 	{
@@ -92,19 +90,10 @@ va_list va
 }
 
 
-__attribute__ ((unused))
 __attribute__ ((noreturn))
 __attribute__ ((format (printf, 6, 0)))
 static void assert_format 
-(
-int id,
-char const * file,
-int line,
-char const * fn,
-char const * exp,
-char const * fmt,
-...
-)
+(int id,char const * file,int line,char const * fn,char const * exp,char const * fmt,...)
 {
 /*
 #ifdef _WIN32
@@ -122,28 +111,5 @@ char const * fmt,
 
 
 
-__attribute__ ((unused))
-__attribute__ ((format (printf, 5, 0)))
-static void trace_format 
-(
-int id,
-char const * file,
-int line,
-char const * fn,
-char const * fmt,
-...
-)
-{
-	va_list list;
-	va_start (list, fmt);
-	
-	fprintf (stderr, TRACE_TCOL_INFO0 "TRACE " TRACE_TCOL_INFO1 "[%04i]" TCOL_RST " ", id);
-	fprintf (stderr, TRACE_TCOL_INFO2 "%s" TRACE_TCOL_INFO3 ":" TCOL_RST, file);
-	fprintf (stderr, TRACE_TCOL_INFO4 "%04i" TCOL_RST " in ", line);
-	fprintf (stderr, TRACE_TCOL_INFO5 "%s" TCOL_RST " ()", fn);
-	fprintf (stderr, ": ");
-	vfprintf (stderr, fmt, list);
-	fprintf (stderr, "\n");
-	fflush (stderr);
-	va_end (list);
-}
+
+
--- test_csc_debug.c
+++ test_csc_debug.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include "csc_crossos.h"
 #include "csc_debug.h"
 
@@ -14,6 +15,55 @@ int main (int argc, char * argv [])
 	ASSERT (argc);
 	ASSERT (argv);
 	csc_crossos_enable_ansi_color();
+
+#if 0
 	foo (NULL);
+#endif
+
+#if 0
+	void * x = (void *)(intptr_t)0x44;
+	ASSERT_NOTNULL(x);
+#endif
+
+#if 0
+	int a = 4;
+	int b = 3;
+	ASSERT_LTI(a, b);
+#endif
+
+#if 0
+	errno = 4;
+	struct s{int alpha;};
+	struct s * a = (struct s *)(intptr_t)0xABC54;
+	struct s * b = (struct s *)(intptr_t)0xABC44;
+	ASSERT_LTP(a, b);
+#endif
+
+#if 0
+	errno = 4;
+	void * a = (void *)(intptr_t)0xABC54;
+	void * b = (void *)(intptr_t)0xABC44;
+	ASSERT_EQP(a, b);
+#endif
+
+#if 0
+	errno = 4;
+	int a = 1;
+	ASSERT_FALSE(a);
+#endif
+
+#if 0
+	errno = 4;
+	unsigned a = 3;
+	ASSERT_POW2(a);
+#endif
+
+#if 1
+	errno = 4;
+	void * a = (void *)(intptr_t)0xABC5431;
+	unsigned x = 2;
+	ASSERT_ALIGN(a, x);
+#endif
+
 	return EXIT_SUCCESS;
 }
