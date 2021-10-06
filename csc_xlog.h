d6202b53368b9245cd593b2f3f987b451b8ac1e2
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2021-10-06 18:01:21 +0200

    change xlog

--- csc_xlog.h
+++ csc_xlog.h
@@ -48,7 +48,7 @@ static char const * xlogcategory_tostr(enum xlogcategory category)
 	switch (category)
 	{
 	case XLOG_GENERAL:
-		return TFG(152,90,133) "GENERAL  " TCOL_RST;
+		return TFG(152,90,133) "" TCOL_RST;
 		break;
 	case XLOG_OPENGL:
 		return TFG(152,90,133) "OPENGL   " TCOL_RST;
@@ -81,7 +81,7 @@ void xlog(int counter, char const * file, int line, char const * func, enum xlog
 {
 	va_list args;
 	va_start (args, format);
-	printf (TFG(100,100,100) "[%i]" TCOL_RST "%s %s " TFG(100,100,100) "%s:%i " TFG(130, 110, 60) "%s() " TCOL_RST, counter, xloglvl_tostr(level), xlogcategory_tostr(category), file, line, func);
+	printf (TFG(100,100,100) "@%04i " TCOL_RST "%s %s " TFG(100,100,100) "%s:%i " TFG(130, 110, 60) "%s() " TCOL_RST, counter, xloglvl_tostr(level), xlogcategory_tostr(category), file, line, func);
 	vprintf (format, args);
 	putc ('\n', stdout);
 	va_end (args);
